b1b3042b100dc564b6bfa303fe6df7aecbb5999f
Saket Khopkar <60461421+SAKET-SK@users.noreply.github.com>
2020-11-09 20:16:49 +0530

    Add files via upload

---
+++ Day 2/ipop.js
@@ -0,0 +1,34 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    let r = readLine();
+    const PI = Math.PI;
+    
+    // Print the area of the circle:
+    console.log(PI*r*r);
+    // Print the perimeter of the circle:
+    console.log(PI*2*r);
+    try {    
