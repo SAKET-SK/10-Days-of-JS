5320145559dc5eef3b609d3ee7383756444afc04
Saket Khopkar <saketkhopkar478@gmail.com>
2020-11-22 11:49:55 +0530

    Add files via upload

---
+++ Day 5/CountObjects.js
@@ -0,0 +1,34 @@
+se strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ * 
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+function getCount(objects) {
+    return objects.filter(item =>item.x===item.y).length;
+}
+
